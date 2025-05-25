44c4ef71c8ff6b9acf7373cc64b2ecf8e34e2004
hene1804 <henriettenamasaya@gmail.com>
2025-05-25 20:52:05 +0300

    make changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const cors = require('cors');
+
+// Fix SSL certificate issue for MongoDB Atlas
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 const app = express();
 const PORT = process.env.PORT || 7000;
 
@@ -15,12 +19,21 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://brianmwenda255:41q8HYCSVjzeiDOG@cluster0.4tg7pi2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsInsecure=true')
+// MongoDB Atlas connection with SSL certificate fix
+const connectionString = 'mongodb+srv://brianmwenda255:41q8HYCSVjzeiDOG@cluster0.4tg7pi2.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0&ssl=true&tlsAllowInvalidCertificates=true&tlsAllowInvalidHostnames=true';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+})
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB Atlas successfully');
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit if database connection fails
   });
 
 app.use('/', routes);
